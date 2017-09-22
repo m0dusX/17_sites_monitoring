a491bf7e7fa8081ac9eb352851d9695988e2e2b6
m0dusX <31633375+m0dusX@users.noreply.github.com>
2017-09-22 20:43:53 +0300

    Update check_sites_health.py

--- check_sites_health.py
+++ check_sites_health.py
@@ -1,11 +1,42 @@
+import sys
+import whois 
+import requests
+import datetime
+from dateutil.relativedelta import relativedelta
+
 def load_urls4check(path):
-    pass
+    with open(path, 'r', encoding="UTF-8") as site_list:
+        return site_list.read().splitlines() 
 
 def is_server_respond_with_200(url):
-    pass
+    try:
+        if requests.get(url).status_code == 200:
+            return True
+        else:
+            return False
+    except:
+        return False
+
 
 def get_domain_expiration_date(domain_name):
-    pass
+    expiration_date = whois.whois(domain_name).expiration_date
+    #Check if expiration_date function returns list. If true, actual exp.
+    #date will be second element in it.
+    if not isinstance(expiration_date, list):
+        return expiration_date
+    else:
+        return expiration_date[1]
 
 if __name__ == '__main__':
-    pass
+    for idx, site in enumerate(load_urls4check(sys.argv[1]), 1):
+        print("{}) {}".format(idx, site))
+        if is_server_respond_with_200(site):
+            print("Status: ONLINE (200)")
+        else:
+            print("Status: OFFLINE")
+        minimum_date = (datetime.datetime.now() + relativedelta(months=+1))
+        if get_domain_expiration_date(site) > minimum_date:
+            print("Domain name is paid for at least 1 month ahead\n")
+        else:
+            print("Caution! Domain name will expire less in 1 month\n")
+    sys.exit()
