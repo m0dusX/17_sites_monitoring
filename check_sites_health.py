d9eb90b9292ad946106fd1ba4041a0169a6e3d7e
m0dusX <31633375+m0dusX@users.noreply.github.com>
2017-10-04 20:53:11 +0300

    Update check_sites_health.py

--- check_sites_health.py
+++ check_sites_health.py
@@ -1,13 +1,16 @@
 import sys
+import argparse
+import datetime
+import requests
 import whois
 from whois.parser import PywhoisError
-import requests
-import datetime
 from dateutil.relativedelta import relativedelta
+
 
 def load_urls4check(path):
     with open(path, 'r', encoding="UTF-8") as site_list:
-        return site_list.read().splitlines() 
+        return site_list.read().splitlines()
+
 
 def is_server_respond_with_200(url):
     try:
@@ -21,15 +24,21 @@ def is_server_respond_with_200(url):
 
 def get_domain_expiration_date(domain_name):
     expiration_date = whois.whois(domain_name).expiration_date
-    #Check if expiration_date function returns list. If true, actual exp.
-    #date will be second element in it.
+    # Check if expiration_date function returns list. If true, actual exp.
+    # date will be second element in it.
     if not isinstance(expiration_date, list):
         return expiration_date
     else:
         return expiration_date[1]
 
+
 if __name__ == '__main__':
-    for idx, site in enumerate(load_urls4check(sys.argv[1]), 1):
+    parser = argparse.ArgumentParser(description='sites health checker')
+    parser.add_argument(
+        'path_to_txt', help='path to txt file with sites')
+    args = parser.parse_args()
+    site_list = args.path_to_txt
+    for idx, site in enumerate(load_urls4check(site_list), 1):
         print("{}) {}".format(idx, site))
         if is_server_respond_with_200(site):
             print("Status: OK (ONLINE)")
@@ -41,7 +50,7 @@ if __name__ == '__main__':
         except PywhoisError:
             print("Domain name was not found in registry\n")
             continue
-        if  expiration_date > minimum_date:
+        if expiration_date > minimum_date:
             print("Domain name is paid for at least 1 month ahead\n")
         else:
             print("Caution! Domain name will expire less in 1 month\n")
